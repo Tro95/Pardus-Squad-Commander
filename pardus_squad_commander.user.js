b17a1e6ebbedc643a838589a817b57c719f441a9
Thomas O'Brien <thomas.r.obrien@btinternet.com>
2023-03-20 12:47:25 +0000

    v0.1.3

--- pardus_squad_commander.user.js
+++ pardus_squad_commander.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name            Pardus Squad Commander
-// @version         0.1.2
+// @version         0.1.3
 // @description     Squad scripts
 // @author          Tro (Artemis)
 // @icon            https://static.pardus.at/img/stdhq/squadrons/fighter_squad_1.png
@@ -10,9 +10,9 @@
 // @grant           GM_setValue
 // @grant           GM_getValue
 // @require         https://raw.githubusercontent.com/Tro95/Pardus-Options-Library/v2.6.0/dist/pardus-options-library.js
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Squad-Commander/v0.1.2/pages/options.js
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Squad-Commander/v0.1.2/pages/squad_main.js
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Squad-Commander/v0.1.2/pages/squad_combat.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Squad-Commander/v0.1.3/pages/options.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Squad-Commander/v0.1.3/pages/squad_main.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Squad-Commander/v0.1.3/pages/squad_combat.js
 // @downloadURL     https://github.com/Tro95/Pardus-Squad-Commander/raw/main/pardus_squad_commander.user.js
 // @updateURL       https://github.com/Tro95/Pardus-Squad-Commander/raw/main/pardus_squad_commander.meta.js
 // ==/UserScript==
